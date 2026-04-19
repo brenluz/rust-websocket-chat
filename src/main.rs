83426804043866d9e02260fdf8a74649709a0e2a
Breno <brenluz@outlook.com>
2026-04-18 23:49:11 -0300

    setup simple websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod message;
 mod db;
-fn main() {
-    println!("Hello, world!");
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new()
+    .route("/", get(|| async { "WebSocket Chat Server" }));
+
+    let listener: tokio::net::TcpListener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app.into_make_service()).await.unwrap();
 }
